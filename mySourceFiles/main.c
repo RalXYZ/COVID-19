107444a3d751bd692e863526043fdc79f37af378
RalXYZ <19510622+RalXYZ@users.noreply.github.com>
2020-04-25 00:52:45 +0800

    Add project files.

---
+++ mySourceFiles/main.c
@@ -0,0 +1,19 @@
+﻿#include "graphics.h"
+#include "extgraph.h"
+#include "strlib.h"
+#include "genlib.h"
+#include <Windows.h>
+#include <stdio.h>
+
+#include "imgui.h"
+
+
+
+void Main() {
+	SetWindowSize(4, 2);
+	SetWindowTitle("一个程序");
+	InitGraphics();
+
+	MovePen(GetWindowWidth() / 2, GetWindowHeight() / 2);
+	DrawTextString("Hello World!");
+}
