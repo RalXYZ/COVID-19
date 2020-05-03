521e1949d0e149d81208fdb2c01c6ba174245cb8
沐含 肖 <2495477644@qq.com>
2020-05-04 02:07:18 +0800

    以防万一，变量先加上

--- myResourceFiles/prediction_model.c
+++ myResourceFiles/prediction_model.c
@@ -1,1 +1,21 @@
+/*
+ * 文件名: prediction_model.c
+ * -------------------------------------
+ * 这个文件实现了基于SEIR模型的疫情预测
+ */
 #include <stdio.h>
+#include <math.h>
+
+static int p;
+static int s;
+static int e;
+static int i;
+static int r;
+static int day;
+
+static double i_infection_rate;
+static double e_infection_rate;
+static double e_turnto_i;
+static double i_touch;
+static double e_touch;
+static double mul_1, mul_2;
