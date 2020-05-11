739b4706dee928066fbd673d1069e8319f9c212b
沐含 肖 <2495477644@qq.com>
2020-05-11 14:53:04 +0800

    增加了一个简单的插入排序计算拐点的函数，用于丰富疫情预测界面

--- mySourceFiles/prediction_model.c
+++ mySourceFiles/prediction_model.c
@@ -15,22 +15,24 @@ static double e_touch;
 static double recovery_rate;
 static double mul_1, mul_2;
 
+static int InflectionDay;//拐点日期
+static int InflectionNumber;//拐点值
+
 double S[102] = { 0 };//易感者
 double E[102] = { 0 };//潜伏者
 double I[102] = { 0 };//感染者
 double R[102] = { 0 };//康复者
 /*
- * 函数名1: SEIR_ENTER_INT
- * 函数名2: SEIR_ENTER_DOUBLE
- * 参数1: day 疫情发生天数
- * 参数2: i_infection_rate 感染者传染率
- * 参数3: e_infection_rate 潜伏者传染率                                                                                                                                                          
- * 参数4: e_turnto_i 潜伏者变为感染者的比率
- * 参数5: i_touch 感染者接触人数
- * 参数6: e_touch 潜伏者接触人数
- * 参数7: recovery_rate 康复概率
- * 参数8: mul_1 参数7*参数10
- * 参数9: mul_2 参数8*参数11
+ * 函数名1: SEIREnterInt
+ * 函数名2: SEIREnterDouble
+ * 参数1: i_infection_rate 感染者传染率
+ * 参数2: e_infection_rate 潜伏者传染率                                                                                                                                                          
+ * 参数3: e_turnto_i 潜伏者变为感染者的比率
+ * 参数4: i_touch 感染者接触人数
+ * 参数5: e_touch 潜伏者接触人数
+ * 参数6: recovery_rate 康复概率
+ * 参数7: mul_1 参数7*参数10
+ * 参数8: mul_2 参数8*参数11
  * -------------------------------------
  * 以下两个函数录入疫情模型的基本参数
  * 参数已经有所预设
@@ -73,4 +75,33 @@ void SEIR()
 		I[K] = I[K - 1] + e_turnto_i * E[K] - recovery_rate * I[K - 1];
 		R[K] = R[K - 1] + recovery_rate * I[K - 1];
 	}
+}
+/*
+ * 函数名: EpidemicInflectionPoint
+ * -------------------------------------
+ * 本函数通过分别调用疫情数据，计算感染人数峰值
+ * 插入排序计算疫情拐点,拐点天数为InflectionDay，值为InflectionNumber
+ */
+int EpidemicInflectionPoint()
+{
+	int t,k;
+	int C[102];
+	for (k = 0; k < 100; k++)
+		C[k] = I[k];
+	int i, j;
+	for (i = 1; i < 100; i++)
+	{
+		t = C[i];
+		for (j = i; j > 0 && t > C[j - 1]; j--)
+			C[j] = C[j - 1];
+		C[j] = t;
+	}
+	for (k = 0; k < 100; k++)
+	{
+		if (C[0] == I[k])
+		{
+			InflectionDay = k + 1;
+		}
+	}
+	C[0] = InflectionNumber;
 }
