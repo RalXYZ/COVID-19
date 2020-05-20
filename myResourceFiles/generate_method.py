833285a592ea60c80d1b6f6198299fb53b247b1b
RalXYZ <19510622+RalXYZ@users.noreply.github.com>
2020-05-20 17:25:13 +0800

    [skip-ci] 修复了资源文件里没有3月31日的漏洞

--- myResourceFiles/generate_method.py
+++ myResourceFiles/generate_method.py
@@ -1,18 +1,30 @@
 from math import *
 
 # 第一份资源文件
-for i in range(0, 30):
+for i in range(0, 31):
 	print("3-" + str(i + 1), end = ' ')
 	print(int(50 * (sin(0.1 * i) + 1)), end = ' ')
 	print(int(50 * (cos(0.3 * i) + 1)), end = ' ')
 	print(int(100 * log(0.1 * i + 1)), end = ' ' )
 	print(int(20 * sqrt(i)), end = '\n' )
+for i in range(0, 18):
+	print("4-" + str(i + 1), end = ' ')
+	print(int(50 * (sin(0.1 * (31 + i)) + 1)), end = ' ')
+	print(int(50 * (cos(0.3 * (31 + i)) + 1)), end = ' ')
+	print(int(100 * log(0.1 * (31 + i) + 1)), end = ' ' )
+	print(int(20 * sqrt(31 + i)), end = '\n' )
 
 # 第二份资源文件
-for i in range(0, 30):
+for i in range(0, 31):
 	print("3-" + str(i + 1), end = ' ')
 	print(int(50 * pow(sin(0.1 * i), 2)), end = ' ')
 	print(int(50 * pow(cos(0.3 * i), 2)), end = ' ')
 	print('20', end = ' ' )
 	print(int(7 * sqrt(i)), end = '\n' )
+for i in range(0, 18):
+	print("4-" + str(i + 1), end = ' ')
+	print(int(50 * pow(sin(0.1 * (31 + i)), 2)), end = ' ')
+	print(int(50 * pow(cos(0.3 * (31 + i)), 2)), end = ' ')
+	print('20', end = ' ' )
+	print(int(7 * sqrt(31 + i)), end = '\n' )
 
